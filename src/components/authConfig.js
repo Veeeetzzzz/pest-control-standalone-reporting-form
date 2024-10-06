6899ff2081c31129fc5dd4ba0a5b46cb333f63a1
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2024-10-06 18:17:20 +0100

    Create authConfig.js

---
+++ src/components/authConfig.js
@@ -0,0 +1,40 @@
+import { Configuration, LogLevel } from '@azure/msal-browser';
+
+export const msalConfig: Configuration = {
+  auth: {
+    clientId: 'YOUR_CLIENT_ID',
+    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID',
+    redirectUri: 'http://localhost:3000'
+  },
+  cache: {
+    cacheLocation: 'sessionStorage',
+    storeAuthStateInCookie: false,
+  },
+  system: {
+    loggerOptions: {
+      loggerCallback: (level, message, containsPii) => {
+        if (containsPii) {
+          return;
+        }
+        switch (level) {
+          case LogLevel.Error:
+            console.error(message);
+            return;
+          case LogLevel.Info:
+            console.info(message);
+            return;
+          case LogLevel.Verbose:
+            console.debug(message);
+            return;
+          case LogLevel.Warning:
+            console.warn(message);
+            return;
+        }
+      }
+    }
+  }
+};
+
+export const loginRequest = {
+  scopes: ['User.Read']
+};
